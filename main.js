e509213eb0da35ea37fdf75fabeff95cd569cedb
tanjiro <92766559+tanjiroTnT@users.noreply.github.com>
2022-09-29 19:25:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw(){
+}
